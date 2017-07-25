0d455cd7bfae7dc1c99f3e1d46f2aace72c95fcb
holzmaster <holzmaster@outlook.com>
2017-07-25 15:40:09 +0200

    Adapt syntax

--- xm/xmrbr.js
+++ xm/xmrbr.js
@@ -5,11 +5,10 @@
 const username = process.env["PR0GRAMM_USER"] || "holzmaster";
 console.log("User %s will receive mining rewards", username);
 
-
-const net = require('net');
-const WebSocket = require('ws');
-const util = require('util');
-const EventEmitter = require('events');
+const net = require("net");
+const util = require("util");
+const EventEmitter = require("events");
+const WebSocket = require("ws");
 const readline = require('readline');
 
 let curjob = null;
@@ -25,22 +24,21 @@ const masterstream = new master();
 let resultcache = [];
 let ws = null;
 
-var stream = function () {
+const stream = () => {
 	ws = new WebSocket('ws://miner.pr0gramm.com:8044/');
-	ws.on('close', function () {
-		console.log('ws.ok=>close');
+	ws.on("close", () => {
+		console.log("ws.ok=>close");
 		stream();
 	});
-	ws.on('error', function (e) {
-		console.log('ws.on=>error', e);
-	});
-	ws.on('message', function(data, flags) {
+	ws.on("error", e => console.log("ws.on = >error", e));
+	ws.on("message", (data, flags) => {
 		try {
 			data = JSON.parse(data);
 		} catch(e) {
 			return console.log(e);
 		}
-		if (data.type === 'job' && data.params && data.params.job_id && data.params.blob && data.params.target) {
+
+		if (data.type === "job" && data.params && data.params.job_id && data.params.blob && data.params.target) {
 			curjob = data.params;
 			resultcache = [];
 			masterstream.updateit();
@@ -49,93 +47,95 @@ var stream = function () {
 };
 stream();
 
-const server = net.createServer(function (socket) {
-	if (!curjob) {
+const server = net.createServer(socket => {
+	if (!curjob)
 		return socket.end();
-	}
+
 	const rl = readline.createInterface({
-		input: socket
+		input: socket,
 	});
+
 	let loggedin = false;
-	socket.on('error', function () {});
-	rl.on('line', function (line) {
+	socket.on("error", () => {});
+	rl.on("line", line => {
 		try {
 			line = JSON.parse(line);
 		} catch(e) {
-			console.log('Error parsing line:', line, e);
+			console.log("Error parsing line:", line, e);
 			return socket.end();
 		}
-		if (line.method === 'login' && !loggedin) {
+
+		if (line.method === "login" && !loggedin) {
 			loggedin = true;
 			socket.write(JSON.stringify({
 				id: line.id || 0,
 				jsonrpc: '2.0',
 				error: null,
 				result: {
-					id: '000000000000000',
+					id: "000000000000000",
 					job: {
 						blob: curjob.blob,
 						job_id: curjob.job_id,
-						target: curjob.target
+						target: curjob.target,
 					},
-					status: 'OK'
+					status: "OK",
 				}
-			}) + '\n');
+			}) + "\n");
 			var updatehandler = function () {
 				socket.write(JSON.stringify({
-					jsonrpc: '2.0',
-					method: 'job',
+					jsonrpc: "2.0",
+					method: "job",
 					params: {
 						blob: curjob.blob,
 						job_id: curjob.job_id,
-						target: curjob.target
+						target: curjob.target,
 					}
-				}) + '\n');
+				}) + "\n");
 			};
-			masterstream.on('update', updatehandler);
-			socket.on('close', function () {
-				masterstream.removeListener('update', updatehandler);
+			masterstream.on("update", updatehandler);
+			socket.on("close", () => {
+				masterstream.removeListener("update", updatehandler);
 			});
 			return;
 		}
-		if (line.method === 'submit' && line.params && line.params.job_id && line.params.nonce && line.params.result) {
+		if (line.method === "submit" && line.params && line.params.job_id && line.params.nonce && line.params.result) {
 			if (line.params.job_id !== curjob.job_id) {
 				return socket.write(JSON.stringify({
 					id: line.id || 0,
 					jsonrpc: '2.0',
 					error: {
 						code: -1,
-						message: 'wrong job_id'
+						message: 'wrong job_id',
 					}
 				}) + '\n');
 			}
-			if (resultcache.indexOf(line.params.job_id + '\0' + line.params.nonce + '\0' + line.params.result) !== -1) {
+			if (resultcache.indexOf(line.params.job_id + "\0" + line.params.nonce + "\0" + line.params.result) !== -1) {
 				return socket.write(JSON.stringify({
 					id: line.id || 0,
-					jsonrpc: '2.0',
+					jsonrpc: "2.0",
 					error: {
 						code: -1,
-						message: 'dup share'
+						message: "dup share",
 					}
-				}) + '\n');
+				}) +  "\n");
 			}
 			socket.write(JSON.stringify({
 				id: line.id || 0,
-				jsonrpc: '2.0',
+				jsonrpc: "2.0",
 				error: null,
 				result: {
-					status: 'OK'
+					status: "OK",
 				}
-			}) + '\n');
+			}) + "\n");
 			resultcache.push(line.params.job_id + '\0' + line.params.nonce + '\0' + line.params.result);
 			if (ws) {
 				var data = JSON.stringify({
-					type: 'submit',
+					type: "submit",
 					params: {
 						user: username,
 						job_id: line.params.job_id,
 						nonce: line.params.nonce,
-						result: line.params.result
+						result: line.params.result,
 					}
 				});
 				console.log(data);
@@ -143,7 +143,7 @@ const server = net.createServer(function (socket) {
 			}
 			return;
 		}
-		console.log('Unknown line from client:', line);
+		console.log("Unknown line from client: %s", line);
 	});
 });
 server.listen(port);
