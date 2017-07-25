589635b95d6ba998af509479ec0b9174b53afb49
holzmaster <holzmaster@outlook.com>
2017-07-25 15:26:55 +0200

    Add defaults

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Starting proxy"
-forever start /xm/xmrbr.js
-
-sleep 5s
-
-echo "Starting miner"
-
+export PR0GRAMM_USER=${PR0GRAMM_USER:-holzmaster}
 export NUM_THREADS_ALT=$(($(grep '^processor' /proc/cpuinfo | wc -l)/2))
 export NUM_THREADS=${NUM_THREADS:-$NUM_THREADS_ALT}
+
+echo "User $PR0GRAMM_USER will receive mining rewards."
+echo "Will use $NUM_THREADS threads."
+
+echo "Starting proxy."
+forever start /xm/xmrbr.js
+
+echo "Waiting 5s for proxy to initialize."
+sleep 5s
+
+echo "Starting miner."
 ./xmrig --print-time=15 --max-cpu-usage=100 -t $NUM_THREADS -a cryptonight -o stratum+tcp://127.0.0.1:12345 -u x -p x
